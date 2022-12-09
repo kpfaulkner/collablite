4e89ce967cc30aadd217a116167eece6f555f560
Ken Faulkner <ken.faulkner@gmail.com>
2022-12-09 22:08:05 +1100

    sqlite add/delete/update

--- pkg/storage/dbsqlite.go
+++ pkg/storage/dbsqlite.go
@@ -40,7 +40,7 @@ func NewDBSQLite(filename string) (*DBSQLite, error) {
 // createTables creates the tables required for storing the documents.
 func createTables(ctx context.Context, conn *sql.Conn) error {
 
-	_, err := conn.ExecContext(ctx, `create table if not exists document (id varchar(50) primary key, object_id varchar(50), property_id varchar(50), value varchar(10000))`)
+	_, err := conn.ExecContext(ctx, `create table if not exists document (id INTEGER PRIMARY KEY AUTOINCREMENT, object_id varchar(50), property_id varchar(50), data varchar(10000))`)
 	if err != nil {
 		log.Printf("unable to create changes table: %v", err)
 		return err
@@ -48,15 +48,51 @@ func createTables(ctx context.Context, conn *sql.Conn) error {
 	return nil
 }
 
-func (db *DBSQLite) Add(objectID string, path string, data []byte) error {
+func (db *DBSQLite) Add(objectID string, path string, data string) error {
+	ctx := context.Background()
+	txn, err := db.conn.BeginTx(ctx, &sql.TxOptions{ReadOnly: false})
+	if err != nil {
+		return fmt.Errorf("unable to create transaction: %w", err)
+	}
+
+	defer txn.Rollback()
+
+	if _, err := txn.ExecContext(ctx, "INSERT INTO document ( object_id, property_id,data) VALUES(?, ?, ?)",
+		objectID, path, data); err != nil {
+		return fmt.Errorf("insert document: %w", err)
+	}
+	txn.Commit()
+
 	return nil
 }
 
 func (db *DBSQLite) Delete(objectID string, path string) error {
+	ctx := context.Background()
+	txn, err := db.conn.BeginTx(ctx, &sql.TxOptions{ReadOnly: false})
+	if err != nil {
+		return fmt.Errorf("unable to create transaction: %w", err)
+	}
+
+	if _, err := txn.ExecContext(ctx, "DELETE FROM document WHERE object_id = ? AND property_id = ?", objectID, path); err != nil {
+		return fmt.Errorf("delete change: %w", err)
+	}
+
 	return nil
 }
 
-func (db *DBSQLite) Update(objectID string, path string, data []byte) error {
+func (db *DBSQLite) Update(objectID string, path string, data string) error {
+	ctx := context.Background()
+	txn, err := db.conn.BeginTx(ctx, &sql.TxOptions{ReadOnly: false})
+	if err != nil {
+		return fmt.Errorf("unable to create transaction: %w", err)
+	}
+	defer txn.Rollback()
+
+	if _, err := txn.ExecContext(ctx, "UPDATE document SET data = ? where object_id = ? and property_id = ?",
+		data, objectID, path); err != nil {
+		return fmt.Errorf("update document of %s: %w", objectID, err)
+	}
+	txn.Commit()
 	return nil
 }
 
